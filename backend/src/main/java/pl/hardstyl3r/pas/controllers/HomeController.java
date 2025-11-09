b6801b1a96797ed13fed1ed956b1d34a906156cd
Aleksander Gencel <johnnylittleplanet@gmail.com>
2025-11-09 17:26:24 +0100

    pas: REST API start

---
+++ backend/src/main/java/pl/hardstyl3r/pas/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package pl.hardstyl3r.pas.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/**")
+    public String home() {
+        return "Library REST API";
+    }
+}
